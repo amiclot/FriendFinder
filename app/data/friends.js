9320f5edb526a5f6ea1d8d66b378144630031891
Adam Miclot <amiclot09@gmail.com>
2017-10-16 14:29:32 -0400

    still working on finding best match and sending to modal

---
+++ app/data/friends.js
@@ -0,0 +1,99 @@
+var friends = [
+    {
+        "name": "Ahmed",
+        "photo": "http://static.tumblr.com/tgyrlv7/2ZDme4986/profile.png",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Crumb",
+        "photo": "https://s3-us-west-2.amazonaws.com/s.cdpn.io/328820/profile/profile-512.jpg?1",
+        "scores":[
+            3,
+            2,
+            3,
+            4,
+            5,
+            3,
+            3,
+            2,
+            4,
+            3
+        ]
+    },
+    {
+        "name": "Aaron",
+        "photo": "https://cdn.pixabay.com/photo/2017/08/23/08/33/cats-eyes-2671903_1280.jpg",
+        "scores":[
+            5,
+            4,
+            5,
+            5,
+            4,
+            3,
+            4,
+            5,
+            3,
+            5
+        ]
+    },
+    {
+        "name": "Jerick",
+        "photo": "http://www.designskilz.com/random-users/images/imageM13.jpg",
+        "scores":[
+            2,
+            3,
+            1,
+            4,
+            3,
+            2,
+            2,
+            4,
+            2,
+            1
+        ]
+    },
+    {
+        "name": "Elijah",
+        "photo": "https://cdn.pixabay.com/photo/2017/08/23/08/01/animals-2671825_1280.jpg",
+        "scores":[
+            4,
+            5,
+            4,
+            4,
+            1,
+            4,
+            4,
+            3,
+            4,
+            4
+        ]
+    },
+    {
+        "name": "Jamison",
+        "photo": "https://i.pinimg.com/originals/dd/7d/f5/dd7df51eabb912d33d6a6f669f9c3a14.jpg",
+        "scores":[
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+        ]
+    },
+]
+module.exports = friends;
---
+++ server.js
@@ -0,0 +1,22 @@
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+
+var app = express();
+var port = process.env.PORT || 3001;
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+
+app.listen(port, function () {
+    console.log("App listening on PORT " + port);
+});
